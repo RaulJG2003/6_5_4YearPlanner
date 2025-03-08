ba5b44a9bb131096f2febf514030ce2347c3e57d
RaulJG2003 <rjorrin1@umbc.edu>
2025-03-08 02:13:52 -0500

    a new csv & popJSON() funciton

--- server/scripts/seed.js
+++ server/scripts/seed.js
@@ -1,15 +1,43 @@
 const mongoose = require("mongoose");
 const connectDB = require("../config/db");
 const Course = require("../models/Course");
+const csvtojson = require("csvtojson"); 
 
-const seedCourses = [
-  { code: "CMSC 201", name: "Computer Science I", credits: 4, prerequisites: [], semesterOffered: ["Fall", "Spring"] },
-  { code: "CMSC 202", name: "Computer Science II", credits: 4, prerequisites: ["CMSC 201"], semesterOffered: ["Spring", "Fall"] },
-  { code: "CMSC 341", name: "Data Structures", credits: 3, prerequisites: ["CMSC 202"], semesterOffered: ["Fall", "Spring"] },
-  { code: "CMSC 313", name: "Assembly", credits: 3, prerequisites: ["CMSC 202"], semesterOffered: ["Fall", "Spring"] },
-];
+var seedCourses = []; // our json
+
+//populates a json 
+function popJSON()
+{
+  const fileName = "classes.csv"; 
+  csvtojson().fromFile(fileName).then((source) =>
+  {
+    //goes through the source which has been populated from the csv converts to rows to put in seedCourses
+    for(var i = 0; i <source.length;i ++)
+    {
+      //the format for the JSON
+      var oneRow ={
+        code: source[i]["Class"],
+        name: source[i]["Name"],
+        prerequisites: source[i]["PreReq"],
+        credits: source[i]["Credits"],
+        attributes: source[i]["Attributes"],
+        semesterOffered:source[i]["Semester Offerd"],
+      }
+      seedCourses.push(oneRow);
+    }
+    console.log("CSV translated!");
+  })
+
+}
+//const seedCourses = [
+  //{ code: "CMSC 201", name: "Computer Science I", credits: 4, prerequisites: [], semesterOffered: ["Fall", "Spring"] },
+  //{ code: "CMSC 202", name: "Computer Science II", credits: 4, prerequisites: ["CMSC 201"], semesterOffered: ["Spring", "Fall"] },
+  //{ code: "CMSC 341", name: "Data Structures", credits: 3, prerequisites: ["CMSC 202"], semesterOffered: ["Fall", "Spring"] },
+  //{ code: "CMSC 313", name: "Assembly", credits: 3, prerequisites: ["CMSC 202"], semesterOffered: ["Fall", "Spring"] },
+//];
 
 const seedDB = async () => {
+  popJSON();
   await connectDB();
   await Course.deleteMany({});
   await Course.insertMany(seedCourses);
